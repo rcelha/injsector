1c87e7c13f083bbacefe5b1c41bd4310b8cc59ad
Rodrigo Corrêa <rcelha@gmail.com>
2016-06-12 13:03:25 +0100

    added node package

--- inject.js
+++ inject.js
@@ -1,10 +1,12 @@
+/* global chrome */
+
 chrome.storage.local.get('injsect-code', function (items) {
   const code = items['injsect-code']
-  const scriptTag = document.createElement ("script");
+  const scriptTag = document.createElement('script')
   scriptTag.text = `
     console.log('[injsect] Injecting loader')
     ${code}
   `
-  scriptTag.async = false;
-  document.documentElement.appendChild(scriptTag);
+  scriptTag.async = false
+  document.documentElement.appendChild(scriptTag)
 })
