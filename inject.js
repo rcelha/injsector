4a656f5a4dd6b69a37b7a0631603c7525618ad36
Rodrigo Corrêa <rcelha@gmail.com>
2016-06-12 18:18:42 +0100

    loading scripts

--- inject.js
+++ inject.js
@@ -1,12 +1,37 @@
-/* global chrome */
+/* global chrome XMLHttpRequest */
 
-chrome.storage.local.get('injsect-code', function (items) {
-  const code = items['injsect-code']
-  const scriptTag = document.createElement('script')
-  scriptTag.text = `
-    console.log('[injsect] Injecting loader')
-    ${code}
-  `
-  scriptTag.async = false
-  document.documentElement.appendChild(scriptTag)
+function loadScript (url, cb, err) {
+  const request = new XMLHttpRequest()
+  request.open('GET', url, true)
+
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+      const resp = request.responseText
+      cb(resp)
+    } else {
+      err(request)
+    }
+  }
+  request.onerror = err
+  request.send()
+}
+
+chrome.storage.local.get('injsect-urls', function (items) {
+  const code = items['injsect-urls']
+  console.log(code)
+  for (let k in code) {
+    const block = code[k]
+    loadScript(block.url,
+      (ret) => {
+        const scriptTag = document.createElement('script')
+        scriptTag.text = ret
+        scriptTag.async = false
+        document.documentElement.appendChild(scriptTag)
+      },
+      (err) => {
+        console.log('Injsect error', err)
+      }
+    )
+  }
 })
