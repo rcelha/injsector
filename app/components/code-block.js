e4daf5c95cef370ba89b5984ed6b1c5b951c243b
Rodrigo Corrêa <rcelha@gmail.com>
2016-07-22 16:21:18 +0100

    Add new block options

--- app/components/code-block.js
+++ app/components/code-block.js
@@ -1,22 +1,27 @@
 const React = require('react')
 
 export class CodeBlock extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
+
+  getStateDict (props) {
+    props = (props === undefined) ? this.props : props
+    return {
       id: props.id,
       url: props.url,
       code: props.code,
-      isCode: props.isCode || '0'
+      isCode: props.isCode || '0',
+      urlMatch: props.urlMatch || '',
+      replaceSrcMatch: props.replaceSrcMatch || '',
+      replaceTextMatch: props.replaceTextMatch || ''
     }
   }
 
+  constructor (props) {
+    super(props)
+    this.state = this.getStateDict(props)
+  }
+
   onCancelClick () {
-    this.setState({
-      url: this.props.url,
-      code: this.props.code,
-      isCode: this.props.isCode
-    })
+    this.setState(this.getStateDict())
   }
 
   onRemoveClick (evt) {
@@ -48,18 +53,31 @@ export class CodeBlock extends React.Component {
 
     return (
       <fieldset>
-        <p>{codeComponent}</p>
-        <p>
-          <button onClick={this.onSaveClick.bind(this)}>Save</button>
-          <button onClick={this.onCancelClick.bind(this)}>Reset</button>
-          <button onClick={this.onRemoveClick.bind(this)}>Remove</button>
-        </p>
         <p>
           <label htmlFor='isCode'>Type</label>
           <select ref='isCode' value={this.state.isCode} onChange={this.onChangeField.bind(this, 'isCode')}>
             <option value='0'>URL</option>
             <option value='1'>Code Block</option>
           </select>
+        </p>
+        <p>{codeComponent}</p>
+        <p>
+          <label htmlFor='urlMatch'>Run only on domains that match:</label>
+          <input ref='urlMatch' type='text' onChange={this.onChangeField.bind(this, 'urlMatch')} value={this.state.urlMatch} />
+        </p>
+        <p>Replace Element Options:</p>
+        <p>
+          <label htmlFor='replaceSrcMatch'>SRC Pattern</label>
+          <input ref='replaceSrcMatch' type='text' onChange={this.onChangeField.bind(this, 'replaceSrcMatch')} value={this.state.replaceSrcMatch} />
+        </p>
+        <p>
+          <label htmlFor='replaceTextMatch'>Content Pattern</label>
+          <input ref='replaceTextMatch' type='text' onChange={this.onChangeField.bind(this, 'replaceTextMatch')} value={this.state.replaceTextMatch} />
+        </p>
+        <p>
+          <button onClick={this.onSaveClick.bind(this)}>Save</button>
+          <button onClick={this.onCancelClick.bind(this)}>Reset</button>
+          <button onClick={this.onRemoveClick.bind(this)}>Remove</button>
         </p>
       </fieldset>
     )
--- inject.js
+++ inject.js
@@ -2,6 +2,36 @@
 
 import {load} from './app/components/storage'
 
+function setUpReplace (block) {
+  const observer = new window.MutationObserver(function (m) {
+    m.forEach(mutation => {
+      if (mutation.addedNodes.length > 0) {
+        mutation.addedNodes.forEach(tag => {
+          if (tag.tagName === 'SCRIPT') {
+            if (block.replaceSrcMatch !== '' && tag.src.search(new RegExp(block.replaceSrcMatch)) === -1) {
+              return
+            }
+            if (block.replaceTextMatch !== '' && tag.innerText.search(new RegExp(block.replaceTextMatch)) === -1) {
+              return
+            }
+            console.log('added a script tag on observer')
+            tag.innerText = ''
+            tag.setAttribute('src', '')
+            const newScript = document.createElement('script')
+            if (block.isCode === '1') {
+              newScript.innerText = block.code
+            } else {
+              newScript.setAttribute('src', block.url)
+            }
+            tag.insertAdjacentElement('afterend', newScript)
+          }
+        })
+      }
+    })
+  })
+  observer.observe(document, { childList: true, subtree: true })
+}
+
 function loadScript (url, cb, err) {
   const request = new XMLHttpRequest()
   request.open('GET', url, true)
@@ -26,16 +56,31 @@ function insertCode (code) {
   document.documentElement.appendChild(scriptTag)
 }
 
+function checkDomain (block) {
+  const urlMatch = (block.urlMatch === undefined) ? '' : block.urlMatch
+  return window.location.href.search(new RegExp(urlMatch)) > -1
+}
+
 load('injsect-urls')
   .then(items => {
     for (let k in items) {
       const block = items[k]
       const code = block.code.trim()
       const url = block.url.trim()
-      if (code.length > 0) {
+      const replaceSrcMatch = block.replaceSrcMatch
+      const replaceTextMatch = block.replaceTextMatch
+
+      if (!checkDomain(block)) {
+        continue
+      }
+
+      if (replaceTextMatch || replaceSrcMatch) {
+        setUpReplace(block)
+      } else if (block.isCode === '1') {
         insertCode(code)
       } else {
         loadScript(url, insertCode, err => console.log('Injsect error', err))
       }
     }
   })
+  .catch(err => console.log('Injsector top-level error', err))
